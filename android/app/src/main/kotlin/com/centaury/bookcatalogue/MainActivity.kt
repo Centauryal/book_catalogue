d299bd956467457f210a22a7defc00bfde1cc8aa
alfacentaury <alfa.arnialfa@gmail.com>
2025-07-04 01:17:49 +0700

    refactor: Fix error package android

---
+++ android/app/src/main/kotlin/com/centaury/bookcatalogue/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.centaury.bookcatalogue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/centaury/bookcatalogue/book_catalogue/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.centaury.bookcatalogue.book_catalogue
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
